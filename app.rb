e8277a26ab3a2138c8625d2b2e49bd55d78e1e2f
Omar Branez <omar.branez@gmail.com>
2021-04-12 13:24:17 -0400

    commit tree 7a4645baaf0821ad4192616fb624223dab68b536

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Omar Branez"
+    end
+
+    get '/hometown' do
+        "My hometown is Astoria, New York"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is 'Wake Up' by Arcade Fire"
+    end
 end
